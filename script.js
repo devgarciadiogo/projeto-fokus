f2b6240a69de5785323e3f312de862ba48317e83
Diogo Garcia <124640510+devgarciadiogo@users.noreply.github.com>
2024-01-13 16:11:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 5
 let intervaloId = null
 
 musica.loop = true
+audioPlay.pause = true;
 
 musicaFocoInput.addEventListener('change' , () => {
     if(musica.paused) {
@@ -66,6 +70,8 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
+        audioTempoFinalizado.play()
+        zerar()
         alert('Tempo Finalizado!')
         return
     }
@@ -73,8 +79,19 @@ const contagemRegressiva = () => {
     console.log('Temporizador: ' + tempoDecorridoEmSegundos)
 }
 
-startPauseBt.addEventListener('click', iniciar)
+startPauseBt.addEventListener('click', iniciarOuPausar)
 
-function iniciar() {
+function iniciarOuPausar() {
+    if(intervaloId){
+        audioPausa.play();
+        zerar()
+        return
+    }
+    audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
